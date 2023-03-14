a8c84661407608eb553549bfeb7c17b4690b56ad
mydearxym <mydearxym@qq.com>
2023-03-14 15:37:41 +0800

    feat(changelog): clean up

--- priv/repo/migrations/20230314071529_clean_up_unsed_thread_tables.exs
+++ priv/repo/migrations/20230314071529_clean_up_unsed_thread_tables.exs
@@ -2,42 +2,6 @@ defmodule GroupherServer.Repo.Migrations.CleanUpUnsedThreadTables do
   use Ecto.Migration
 
   def change do
-    alter table(:cited_artiments) do
-      remove(:meetup_id)
-    end
-
-    alter table(:articles_join_tags) do
-      remove(:meetup_id)
-    end
-
-    alter table(:abuse_reports) do
-      remove(:meetup_id)
-    end
-
-    alter table(:article_collects) do
-      remove(:meetup_id)
-    end
-
-    alter table(:article_upvotes) do
-      remove(:meetup_id)
-    end
-
-    alter table(:pinned_comments) do
-      remove(:meetup_id)
-    end
-
-    alter table(:comments) do
-      remove(:meetup_id)
-    end
-
-    alter table(:articles_users_emotions) do
-      remove(:meetup_id)
-    end
-
-    alter table(:pinned_articles) do
-      remove(:meetup_id)
-    end
-
     drop_if_exists table("cms_repo_builders")
     drop_if_exists table("meetup_documents")
     drop_if_exists table("cms_meetups")
