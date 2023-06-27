85516ec3b8b0c2fc036c9dfa053967c18667eea0
mydearxym <mydearxym@qq.com>
2023-06-27 11:58:14 +0800

    refactor: baseinfo workflow

--- lib/groupher_server_web/schema/cms/mutations/dashboard.ex
+++ lib/groupher_server_web/schema/cms/mutations/dashboard.ex
@@ -30,10 +30,10 @@ defmodule GroupherServerWeb.Schema.CMS.Mutations.Dashboard do
       dashboard_args(:seo)
 
       middleware(M.Authorize, :login)
-      middleware(M.Passport, claim: "cms->community.update")
+      # middleware(M.Passport, claim: "cms->community.update")
 
       # middleware(M.PublishThrottle)
-      middleware(M.PublishThrottle, interval: 3, hour_limit: 15, day_limit: 30)
+      # middleware(M.PublishThrottle, interval: 3, hour_limit: 15, day_limit: 30)
       resolve(&R.CMS.update_dashboard/3)
     end
 
